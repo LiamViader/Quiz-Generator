e9509845645c5a0d23f4207f575dcc9b4cc5b801
LiamViader <liam.viadr@gmail.com>
2024-06-04 14:10:24 +0200

    Afegit header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react';
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/home';
 
 function App() {
-  const [data, setData] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        // Hacemos la llamada a la API pública de JSONPlaceholder
-        const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-        if (response.ok) {
-          const jsonData = await response.json();
-          setData(jsonData);
-        } else {
-          throw new Error('Error al cargar los datos');
-        }
-      } catch (error) {
-        console.error('Error:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  return (
-    <div>
-      <h1>API Example</h1>
-      {data ? (
-        <div>
-          <h2>Resultados de la API:</h2>
-          <pre>{JSON.stringify(data, null, 2)}</pre>
-        </div>
-      ) : (
-        <p>Cargando...</p>
-      )}
-    </div>
-  );
+  return <Routes>
+    <Route path="/" element={<Home/>}/>
+  </Routes>
+  
 }
 
 export default App
