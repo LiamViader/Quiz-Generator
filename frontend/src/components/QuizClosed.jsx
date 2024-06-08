bc94500998b9bd01459c15c1ef2e71e8d8ca27e7
LiamViader <liam.viadr@gmail.com>
2024-06-08 19:18:34 +0200

    quiz closed acabats

--- frontend/src/components/QuizClosed.jsx
+++ frontend/src/components/QuizClosed.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, CircularProgress, Typography, Menu, MenuItem, IconButton } from '@mui/material';
+import { Box, CircularProgress, Typography, Menu, MenuItem, IconButton, ButtonBase} from '@mui/material';
 import QuizIcon from '@mui/icons-material/Quiz';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
@@ -16,9 +16,13 @@ function QuizClosed({quiz}){
         { id: 3, label: 'Option 3' }
     ];
 
-    // Función para abrir el menú
-    const handleClick = (event) => {
+    const handleClickMenu = (event) => {
+        event.stopPropagation();
         setAnchorEl(event.currentTarget);
+    };
+
+    const handleClickQuiz = (event) => {
+        console.log("clicat");
     };
 
     // Función para cerrar el menú
@@ -26,12 +30,6 @@ function QuizClosed({quiz}){
         setAnchorEl(null);
     };
 
-    // Función para manejar la selección de una opción
-    const handleMenuItemClick = (option) => {
-        console.log('Selected:', option.label);
-        // Lógica adicional aquí (actualizar estado, hacer una solicitud, etc.)
-        handleClose();
-    };
 
     return(
         <Box
@@ -41,7 +39,7 @@ function QuizClosed({quiz}){
         gap='0.2rem'
         py='0.5rem'
         pl='0.6rem'
-        sx={{boxShadow:'2px 2px 2px gray', borderRadius: '1.1rem', backgroundColor:'#051923', color:'white', flex: '1 1 auto', marginRight: '2%', marginTop:'2%', maxWidth: quiz.loading ? '5rem' : '12rem', maxHeight:'1.5rem', overflow: 'hidden'}}
+        sx={{boxShadow:'0px 2px 5px gray', borderRadius: '1.1rem', backgroundColor:'#051923', color:'white', flex: '1 1 auto', marginRight: '2%', marginTop:'2%', maxWidth: quiz.loading ? '5rem' : '12rem', maxHeight:'1.5rem', overflow: 'hidden'}}
       >
         {quiz.loading ? 
             <div style={{paddingRight:'0.5rem', width:'2rem'}}>
@@ -50,18 +48,20 @@ function QuizClosed({quiz}){
             
             :
             <div style={{display: 'flex', overflow: 'hidden', width:'100%'}}>
-                <div style={{ display:'flex', overflow: 'hidden', marginTop:'auto', marginBottom:'auto'}}>
-                    <QuizIcon sx={{color: '#7fffd4',  }} />
-                </div>
-                <div style={{ flex: '2', display: 'flex', alignItems:'center', overflow: 'hidden', marginLeft:'0.3rem'}}>
-                    <Typography sx={{flexGrow:'1', fontWeight: 'bold', fontSize:'0.8rem', overflow:'hidden', textOverflow:'ellipsis', whiteSpace: 'nowrap'}}> {quiz.quiz.name}</Typography>
-                </div>
+                <ButtonBase onClick={handleClickQuiz} sx={{display:'flex', overflow: 'hidden'}}>
+                    <div style={{ display:'flex', overflow: 'hidden', marginTop:'auto', marginBottom:'auto'}}>
+                        <QuizIcon sx={{color: '#7fffd4',  }} />
+                    </div>
+                    <div style={{ flex: '2', display: 'flex', alignItems:'center', overflow: 'hidden', marginLeft:'0.3rem'}}>
+                        <Typography sx={{flexGrow:'1', fontWeight: 'bold', fontSize:'0.8rem', overflow:'hidden', textOverflow:'ellipsis', whiteSpace: 'nowrap'}}> {quiz.quiz.name}</Typography>
+                    </div>
+                </ButtonBase>
+
                 <div style={{display:'flex', justifyContent: 'flex-end', marginLeft:'auto', alignItems:'left', overflow: 'hidden'}}>
                     <IconButton
                         aria-controls="simple-menu"
                         aria-haspopup="true"
-                        onClick={handleClick}
-                        endIcon={<ArrowDropDownIcon />}
+                        onClick={handleClickMenu}
                         sx={{marginLeft:'auto'}}
                     >
                         <MoreVertIcon sx={{color:'white', marginLeft:'auto'}}/>
