0b070ff360dd54fa16e8e169f22577d6d00d21aa
LiamViader <liam.viadr@gmail.com>
2024-06-08 22:19:47 +0200

    prova amb generator conectat a openai

---
+++ backend/src/generator/generateQuizMessage.ts
@@ -0,0 +1,38 @@
+const { AzureOpenAI } = require("openai");
+
+// Load the .env file if it exists
+const dotenv = require("dotenv");
+dotenv.config();
+
+// You will need to set these environment variables or edit the following values
+const endpoint = process.env["AZURE_OPENAI_ENDPOINT"] || "https://quizgenerator-openai-prod.openai.azure.com/";
+const apiKey = process.env["AZURE_OPENAI_API_KEY"] || "5161a8db7df9440998b4f7d5f5da66da";
+const apiVersion = "2024-05-01-preview";
+const deployment = "gpt4"; //This must match your deployment name.
+require("dotenv/config");
+
+export async function generateQuizMessage(): Promise<string> {
+
+  const client = new AzureOpenAI({ endpoint, apiKey, apiVersion, deployment });
+  try{
+    const result = await client.chat.completions.create({
+      messages: [
+      { role: "system", content: "You are a helpful assistant." },
+      { role: "user", content: "Does Azure OpenAI support customer managed keys?" },
+      { role: "assistant", content: "Yes, customer managed keys are supported by Azure OpenAI?" },
+      { role: "user", content: "Do other Azure AI services support this too?" },
+      ],
+      model: "",
+    });
+  
+    if (result.choices && result.choices.length > 0) {
+      return result.choices[0].message.content;
+    } else {
+      throw new Error('No response choices available');
+    }
+  } catch(error){
+    console.error('Error al generar el mensaje:', error);
+    throw new Error('Error al generar el mensaje');
+  }
+
+}
--- backend/src/generator/generator.controller.ts
+++ backend/src/generator/generator.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { generateQuizMessage } from './generateQuizMessage';
 
 @Controller('generator')
-export class GeneratorController {}
+export class GeneratorController {
+
+    @Get() //per provar si hi ha connexio amb openai, retorna missatge generat per gpt
+    async simplePrompt() {
+        try {
+            const generatedMessage = await generateQuizMessage(); // Llama a la función para generar el mensaje
+            return generatedMessage;
+          } catch (error) {
+            // Maneja errores y proporciona retroalimentación adecuada
+            console.error('Error en el controlador:', error);
+            return 'Error al generar el mensaje';
+          }
+    }
+
+}
