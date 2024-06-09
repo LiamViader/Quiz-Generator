e0c94ada67c1727a453626fff6ce2fbbf8355be5
LiamViader <liam.viadr@gmail.com>
2024-06-09 21:55:31 +0200

    backend generar quiz acabat

--- backend/src/generator/generator.service.ts
+++ backend/src/generator/generator.service.ts
@@ -2,7 +2,7 @@ import { Injectable, HttpException,HttpStatus } from '@nestjs/common';
 import { GeneratorRequestDto } from './dto/generator-request.dto';
 import { constructQuizPrompt } from './utils/constructQuizPrompt';
 import { generateQuizChoicesOpenAI } from './utils/generateQuizOpenAI';
-import { constructQuizFromChoice } from './utils/constructQuizFromChoice';
+import { constructQuestionsFromChoice } from './utils/constructQuizFromChoice';
 
 @Injectable()
 export class GeneratorService {
@@ -11,19 +11,30 @@ export class GeneratorService {
         const difficulty=createRequestDto.difficulty;
         const numberQuestions=createRequestDto.numberQuestions;
         const language=createRequestDto.language;
+        const privacy=createRequestDto.privacy;
+        let name=createRequestDto.name;
+        if (name=="") name="Untitled";
 
         const prompt=constructQuizPrompt(topic,difficulty,numberQuestions,language);
         
         try{
             const choices=await generateQuizChoicesOpenAI(prompt,1);
-            const quiz=constructQuizFromChoice(choices[0]);
+            const questions=constructQuestionsFromChoice(choices[0]);
+            const quiz={
+                solved: false,
+                privacy: privacy,
+                name: name,
+                topic: topic,
+                id:2,
+                questions: questions
+            };
             return {
                 status: 'success',
                 message: 'Quiz generated successfully',
                 data: quiz,
               };
         } catch(error){
-            throw new HttpException('Failed to generate quiz', HttpStatus.INTERNAL_SERVER_ERROR);
+            throw new HttpException('Failed to generate quiz:'+error.message, HttpStatus.INTERNAL_SERVER_ERROR);
         }
         
     }
--- backend/src/generator/utils/constructQuizFromChoice.ts
+++ backend/src/generator/utils/constructQuizFromChoice.ts
@@ -1,7 +1,50 @@
+import { UsernamePasswordCredential } from "@azure/identity";
 import { repl } from "@nestjs/core";
 
-export function constructQuizFromChoice(choices:Array<any[]>):string{ 
+function safe(choice:any):boolean{
+    return choice.hate=="safe" && choice.self_harm=="safe" && choice.sexual=="safe" && choice.violence=="safe";
+}
 
-    return "adsa";
+function formattedQuestions(questions:Array<any>):boolean{
+    if(questions.length<=0) return false;
+    
+    let firstLen=questions[0].answers.length;
+    for (let i=0; i<questions.length; i++){
+        if(questions[i].answers.length!=firstLen) return false;
+        if(questions[i].correctAnswer<0 || questions[i].correctAnswer>=questions[i].answers.length)return false;
+    }
+
+    return true;
+}
+
+export function constructQuestionsFromChoice(choice:any):Array<any>{ 
+    if(safe(choice.content_filter_results)) throw new Error('content not safe');
+    const quizMessageWithoutNewLines = choice.message.content.replace(/\n/g, "");
+    if(choice.message.content=="NA") throw new Error('topic dont make sense');
+    const questions = quizMessageWithoutNewLines.split(/\d+\.\s/).filter(Boolean).map(question => question.replace(/^\s+/,""));
+    const finalQuestions=[]
+    for(let i=0; i<questions.length;i++) {
+        let answers=questions[i].split(/[a-z]\) /).filter(Boolean);
+        let correctAnswerSplit=answers[answers.length-1].split(/CA\) /).filter(Boolean);
+        let correctAnswer=correctAnswerSplit[correctAnswerSplit.length-1];
+        correctAnswer.replace(/CA\) /,"");
+        answers.map(question => question.replace(/[a-z]\) /));
+        let finalAnswers=[...answers.slice(1,-1),correctAnswerSplit[0]];
+        const AsciiA = 'a'.charCodeAt(0);
+        let characterAnswer=correctAnswer.charAt(0);
+        const AsciiSol = characterAnswer.charCodeAt(0);
+        let posCorrectAnswer=AsciiSol-AsciiA;
+        let actualFinalQuestion={
+            question: answers[0],
+            answers: finalAnswers,
+            userAnswer: null,
+            correctAnswer: posCorrectAnswer
+        };
+        finalQuestions.push(actualFinalQuestion);
+    }
+    
+    if(!formattedQuestions(finalQuestions)) throw new Error('something went wrong while formatting the quiz');
+    
+    return finalQuestions;
   
 }
