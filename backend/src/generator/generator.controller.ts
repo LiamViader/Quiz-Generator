49e6f16dcc205e2d65a7f367d93ca0cbd945565d
LiamViader <liam.viadr@gmail.com>
2024-06-10 18:04:27 +0200

    sdad

--- backend/src/generator/generator.controller.ts
+++ backend/src/generator/generator.controller.ts
@@ -1,5 +1,4 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
-import { generateQuizMessage } from './generateQuizMessage';
 import { GeneratorRequestDto } from './dto/generator-request.dto';
 import { GeneratorService } from './generator.service';
 
@@ -7,17 +6,6 @@ import { GeneratorService } from './generator.service';
 export class GeneratorController {
     constructor(private readonly generatorService: GeneratorService) {}
     
-    @Get() //per provar si hi ha connexio amb openai, retorna missatge generat per gpt
-    async simplePrompt() {
-        try {
-            const generatedMessage = await generateQuizMessage(); // Llama a la función para generar el mensaje
-            return generatedMessage;
-          } catch (error) {
-            // Maneja errores y proporciona retroalimentación adecuada
-            console.error('Error en el controlador:', error);
-            return 'Error al generar el mensaje';
-          }
-    }
 
     @Post('generate-quiz')
     async create(@Body() createRequestDto: GeneratorRequestDto) {
