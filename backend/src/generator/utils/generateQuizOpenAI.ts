0d0018c25cf56940b71dfbd7b3c8ff4ec8f16ae2
LiamViader <liam.viadr@gmail.com>
2024-06-11 23:00:41 +0200

    Update generateQuizOpenAI.ts

--- backend/src/generator/utils/generateQuizOpenAI.ts
+++ backend/src/generator/utils/generateQuizOpenAI.ts
@@ -2,6 +2,7 @@ const { AzureOpenAI } = require("openai");
 const apiVersion = "2024-02-01";
 const deployment = "gpt4";
 
+
 export async function generateQuizChoicesWithRetry(prompt:any[], nChoices:number, maxRetries:number = 3, endpoint:string, apiKey:string): Promise<any[]> {
     let retryCount = 0;
     let lastError=null;
