fd8229231bb1bc4755de00400fe2d52b0115640f
LiamViader <viaderliam.dev@gmail.com>
2026-02-06 18:46:17 +0100

    Update LimitAnnouncement.jsx

--- frontend/src/components/LimitAnnouncement.jsx
+++ frontend/src/components/LimitAnnouncement.jsx
@@ -48,7 +48,7 @@ function LimitAnnouncement({ user, login }) {
                   }}
                   theme="outline"
                   shape="pill"
-                  size="large"
+                  size="medium"
                   sx={{ backgroundColor: '#7fffd4' }}
                 />
               </Box>
