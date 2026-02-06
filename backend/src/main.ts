afaad53c1f56ff0d496dbaf634380b454a2b5fa6
LiamViader <viaderliam.dev@gmail.com>
2026-02-06 17:25:38 +0100

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // Enable CORS for frontend integration
+
+  app.enableCors({
+    origin: process.env.NODE_ENV === 'production'
+      ? 'https://quiz-generator.liamviader.com'
+      : 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   const port = process.env.PORT || 8080;
 
   app.useGlobalPipes(new ValidationPipe({
